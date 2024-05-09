9b801fdbea669ab9dfadb88d44591c59aff25188
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-05-09 14:28:45 +0200

    I worked with error handliling that checks if inputs are emty or not

--- DJS-2/scripts.js
+++ DJS-2/scripts.js
@@ -4,6 +4,12 @@ const result = document.querySelector("[data-result]");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
-});
+  const dividend = entries.get("dividend");
+  const divider = entries.get("divider");
+
+  // Error handling: check if inputs are empty
+  if (!dividend || !divider) {
+      result.classList.add("error-message")
+      result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+      return;
+  }
