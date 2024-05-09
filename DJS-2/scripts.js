3915a541d1b40d4e9b9f7f27686ee0754d70dcf5
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-05-09 14:33:27 +0200

    I used regex to make sure It's only numbers no alphabets if not it must throw an error massage

--- DJS-2/scripts.js
+++ DJS-2/scripts.js
@@ -13,3 +13,28 @@ form.addEventListener("submit", (event) => {
       result.innerText = "Division not performed. Both values are required in inputs. Try again.";
       return;
   }
+
+   // Error handling: check if inputs contain invalid characters
+   const validInputRegex = /^[0-9]+$/;
+   if (!validInputRegex.test(dividend) || !validInputRegex.test(divider)) {
+       result.classList.add("critical-error")
+       result.innerText = "Something critical went wrong. Please reload the page.";
+       return;
+   }
+
+   try {
+       // Force Number conversion
+       const dividendNumber = Number(dividend);
+       const divisorNumber = Number(divider);
+
+       // Check for specific "YOLO" and "+++" input case
+       if (dividend === "YOLO" && divider === "+++") {
+           result.innerText = "Something critical went wrong. Please reload the page.";
+           return;
+       }
+
+       // Check for other numeric errors
+       if (isNaN(dividendNumber) || isNaN(divisorNumber) || divisorNumber === 0) {
+           throw new Error("Invalid input: Division by zero or non-numeric value provided.");
+       }
+
